5006a901dfc3eb22dcad5aadc6356ca93f85ed16
salva09 <salva.hg01@gmail.com>
2020-05-12 12:34:08 -0500

    Change: Tab token for new variable token

--- src/lexer/Token.kt
+++ src/lexer/Token.kt
@@ -17,6 +17,6 @@ const val MINUS = 13
 const val LINE_BREAK = 14
 const val SEMICOLON = 15
 const val WHITESPACE = 16
-const val TAB = 17
+const val VARIABLE_NOT_VALID = 17
 
 data class Token(var token: Int = 0, var sequence: String = "")
