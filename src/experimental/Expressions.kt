e7b5e075ffe8ebffdb8bfc082e110d2ba82876f3
salva09 <salva.hg01@gmail.com>
2020-05-18 22:01:34 -0500

    Experimental: Postfix on-the-fly validation

---
+++ src/experimental/Expressions.kt
@@ -0,0 +1,94 @@
+import java.util.LinkedList
+import java.util.Stack
+import kotlin.math.pow
+
+fun main() {
+    println("Enter an expression: ")
+    val input = readLine()!!
+    val tokens = LinkedList<Char>()
+    for (char in input) {
+        if (char != ' ') {
+            tokens.add(char)
+        }
+    }
+    try {
+        println("Result: ${evaluate(tokens)}")
+    } catch (ex: ArithmeticException) {
+        println(ex.localizedMessage)
+    } catch (ex: Exception) {
+        println(ex.localizedMessage)
+    }
+}
+fun evaluate(tokens: LinkedList<Char>): Stack<Int> {
+    val values = Stack<Int>()
+    val operators = Stack<Char>()
+    var wasOperator = true
+
+    while (tokens.first != ';' && !tokens.isEmpty()) {
+        println("Values: $values Operators: $operators")
+        if (tokens.first == '(') {
+            operators.add(tokens.first)
+        } else {
+            if (tokens.first == ')') {
+                while (operators.peek() != '(' && !operators.isEmpty()) {
+                    val number1 = values.pop()
+                    val number2 = values.pop()
+                    val operation = operators.pop()
+                    val result = operate(number2, number1, operation)
+                    values.add(result)
+                }
+                operators.pop()
+            } else {
+                if (wasOperator && tokens.first == '-') {
+                    values.add(-1)
+                    operators.add('*')
+                } else {
+                    if (tokens.first.isDigit()) {
+                        values.add(tokens.first.toString().toInt())
+                    } else {
+                        while (!operators.isEmpty() && hierarchy(tokens.first) <= hierarchy(operators.peek())) {
+                            if (operators.peek() != '(') {
+                                val number1 = values.pop()
+                                val number2 = values.pop()
+                                val operation = operators.pop()
+                                val result = operate(number2, number1, operation)
+                                values.add(result)
+                            }
+                        }
+                        operators.add(tokens.first)
+                    }
+                }
+            }
+        }
+        wasOperator = !tokens.first.isDigit()
+        tokens.pop()
+    }
+    while (!operators.isEmpty()) {
+        println("Values: $values Operators: $operators")
+        val number1 = values.pop()
+        val number2 = values.pop()
+        val operation = operators.pop()
+        val result = operate(number2, number1, operation)
+        values.add(result)
+    }
+    return values
+}
+
+fun operate(a: Int, b: Int, operation: Char) = when (operation) {
+    '+' -> a + b
+    '-' -> a - b
+    '*' -> a * b
+    '/' -> if (b != 0) a / b else throw ArithmeticException("Divide by zero cannot be possible")
+    '^' -> a.toFloat().pow(b).toInt()
+    else -> throw Exception("Operator not recognized")
+}
+
+fun hierarchy(operation: Char) = when (operation) {
+    '+' -> 1
+    '-' -> 1
+    '*' -> 2
+    '/' -> 2
+    '^' -> 3
+    '(' -> 0
+    else -> throw Exception("Operator not recognized")
+}
