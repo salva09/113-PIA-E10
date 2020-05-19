5aa482e69ff54b1835dce536ee72da747d9b3295
salva09 <salva.hg01@gmail.com>
2020-05-19 17:44:49 -0500

    Change: MULT_DIV const atomized

--- src/lexer/Token.kt
+++ src/lexer/Token.kt
@@ -12,11 +12,12 @@ const val ASSIGNATION = 8
 const val NUMBER = 9
 const val PLUS = 10
 const val MINUS = 11
-const val MULT_DIV = 12
-const val RAISED = 13
-const val LINE_BREAK = 14
-const val SEMICOLON = 15
-const val WHITESPACE = 16
-const val VARIABLE_NOT_VALID = 17
+const val MULT = 12
+const val DIV = 13
+const val RAISED = 14
+const val LINE_BREAK = 15
+const val SEMICOLON = 16
+const val WHITESPACE = 17
+const val VARIABLE_NOT_VALID = 18
 
 data class Token(var token: Int = 0, var sequence: String = "")
