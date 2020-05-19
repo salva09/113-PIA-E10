5962ff05710f0ac44a6f9b9b62cbbafb5be777b6
salva09 <salva.hg01@gmail.com>
2020-05-19 17:43:35 -0500

    Change: String concatenation change to multiline string

--- src/view/About.kt
+++ src/view/About.kt
@@ -5,13 +5,14 @@ import javax.swing.JCheckBox
 import javax.swing.JOptionPane
 
 fun welcome(): Boolean {
-    val message: String = "<html>" +
-            "Thank you for using my program! <br>" +
-            "I hope that you don't face any bugs. <br>" +
-            "You have some file examples with this program. <br><br>" +
-            "Tip: If you don't know how to use this program <br>" +
-            "go to About: How to use." +
-            "</html>"
+    val message: String = """
+        |Thank you for using my program!
+        |I hope that you don't face any bugs.
+        |You have some file examples with this program.
+        |
+        |Tip: If you don't know how to use this program
+        |go to About: How to use.
+        """.trimMargin()
     val checkbox = JCheckBox("Show at startup")
     checkbox.isSelected = true
     val params = arrayOf<Any>(message, checkbox)
@@ -21,23 +22,28 @@ fun welcome(): Boolean {
 }
 
 fun howToUse() {
-    val message: String = "<html>" +
-            "This is a code editor for a specific language, it has <br>" +
-            "a syntax highlighting and a code completion. <br><br>" +
-            "Code completion: <br>" +
-            "Press ctlr + space to use the code completion. <br><br>" +
-            "File options: <br>" +
-            "Open -> Pop ups a menu to select and open a file. <br>" +
-            "Save -> Saves the current opened file. <br>" +
-            "Exit -> Closes the window and exits the program. <br><br>" +
-            "Edit options: <br>" +
-            "Common edit options like everyone knows. <br><br>" +
-            "Analyze option: <br>" +
-            "Analyze language -> Analyzes the code using a <br>" +
-            "recursive descent parser. <br><br>" +
-            "There is a doc with the information of the language, <br>" +
-            "give it a read. <br>" +
-            "<html>"
+    val message: String = """
+        |This is a code editor for a specific language, it has
+        |a syntax highlighting and a code completion.
+        |
+        |Code completion:
+        |Press ctlr + space to use the code completion.
+        |
+        |File options:
+        |Open -> Pop ups a menu to select and open a file.
+        |Save -> Saves the current opened file.
+        |Exit -> Closes the window and exits the program.
+        |
+        |Edit options:
+        |Common edit options like everyone knows.
+        |
+        |Analyze option:
+        |Analyze language -> Analyzes the code using a
+        |recursive descent parser.
+        |
+        |There is a doc with the information of the language,
+        |give it a read.
+        """.trimMargin()
     JOptionPane.showMessageDialog(null, message, "How to use", JOptionPane.QUESTION_MESSAGE)
 }
 
@@ -46,10 +52,11 @@ fun aboutTeam() {
             "<h1> Automata's theory. </h1> <br>" +
             "<h3> Teacher: Yazmany Jahaziel Guerrero Ceja. </h3> <br>" +
             "<h3> Team 10 </h3> <br>" +
-            "Team's members: <br><br>" +
+            "Team's members: <br>" +
+            "<br>" +
             "Salvador Armando Hernández García 1860667 <br>" +
             "Edson Yael García Silva 1863860 <br>" +
-            "María Eugenia Concepción Flores Rodríguez 1818538 <br> <br>" +
+            "María Eugenia Concepción Flores Rodríguez 1818538" +
             "</html>"
     JOptionPane.showMessageDialog(null, message, "Info", JOptionPane.INFORMATION_MESSAGE)
 }
